67d15a82dd3020a72c5cbc3a162483105c09f3cc
Sonny3fingers <77343092+Sonny3fingers@users.noreply.github.com>
2023-01-11 20:15:55 +0100

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrX2OKUgUpQ5iutZMlz4jpd-Fy3bDS8Yo",
+  authDomain: "chat-free-7388b.firebaseapp.com",
+  projectId: "chat-free-7388b",
+  storageBucket: "chat-free-7388b.appspot.com",
+  messagingSenderId: "761259326833",
+  appId: "1:761259326833:web:2dcca7415fdbbf828e4ad0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
